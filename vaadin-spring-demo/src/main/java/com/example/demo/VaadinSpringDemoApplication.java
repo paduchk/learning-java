1d67f530c37ab157ba2a3467baec35fcbe53b0e7
paduchk <paduchk@interia.pl>
2017-07-15 17:35:05 +0200

    initialization

---
+++ vaadin-spring-demo/src/main/java/com/example/demo/VaadinSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSpringDemoApplication.class, args);
+	}
+}

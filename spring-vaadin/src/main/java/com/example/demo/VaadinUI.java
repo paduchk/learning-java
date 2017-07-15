67976377bc5d72746a649215138570515bdab0f5
paduchk <paduchk@interia.pl>
2017-07-15 13:12:53 +0200

    completed UI

--- spring-vaadin/src/main/java/com/example/demo/VaadinUI.java
+++ spring-vaadin/src/main/java/com/example/demo/VaadinUI.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
@@ -22,30 +24,53 @@ public class VaadinUI extends UI {
 	 * 
 	 */
 	private static final long serialVersionUID = -6493427860323504176L;
-	CustomerRepository repo;
-	Grid<Customer> grid;
-	TextField filter = new TextField();
+	
+	private final CustomerRepository repo;
+	
+	private final CustomerEditor editor;
+	
+	final Grid<Customer> grid;
+	
+	final TextField filter;
+	
+	private final Button addNewBtn;
 	
 	@Override
 	protected void init(VaadinRequest request) {
-		// TODO Auto-generated method stub
-		setContent(new Button("Click Me",e-> Notification.show("Hello Spring+Vaadin User")));
-		filter.setPlaceholder("Filter by last name");
-		filter.setValueChangeMode(ValueChangeMode.LAZY);
-		filter.addValueChangeListener(e-> listCustomers(e.getValue()));
-		VerticalLayout mainLayout = new VerticalLayout(filter, grid);
+		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+		VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
 		setContent(mainLayout);
+
+		grid.setHeight(300,Unit.PIXELS);
+		
+		filter.setValueChangeMode(ValueChangeMode.LAZY);
+		filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+		
+		grid.asSingleSelect().addValueChangeListener(e -> {
+			editor.editCustomer(e.getValue());
+		});
+		
+		addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("","")));
+		
+		editor.setChangeHandler(() -> {
+			editor.setVisible(false);
+			listCustomers(filter.getValue());
+		});
+		
 		listCustomers(null);
+		
 	}
 
 	/**
 	 * @param repo
 	 */
 	@Autowired
-	public VaadinUI(CustomerRepository repo) {
-		super();
+	public VaadinUI(CustomerRepository repo, CustomerEditor editor) {
 		this.repo = repo;
+		this.editor = editor;
 		this.grid = new Grid<>(Customer.class);
+		this.filter = new  TextField();
+		this.addNewBtn = new Button("New Customer",FontAwesome.PLUS_CIRCLE);
 	}
 	
 	private void listCustomers(String filterText) {
